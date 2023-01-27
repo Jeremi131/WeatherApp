c2219e4c49833bae7010021d722f708f4ce8ab48
Jeremi131 <jeremijared10@gmail.com>
2023-01-27 19:02:26 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.css'
+import WeatherCard from './components/WeatherCard'
+
+function App() {
+
+  const [coords, setCoords] = useState()
+  const [weather, setWeather] = useState()
+  const [temperature, setTemperature] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+
+
+  useEffect(() => {
+    const success = pos => {
+      const obj = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+      }
+      setCoords(obj)
+    }
+    navigator.geolocation.getCurrentPosition(success) 
+  }, [])
+
+
+  useEffect(() => {
+    if (coords) {
+      const APIKey = 'ff907925cdb3c2beaf3ca900c7e202e5'
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKey}`
+
+      axios.get(url)
+        .then(res => {
+           setWeather(res.data)
+           const obj = {
+              celsius: (res.data.main.temp - 273.15).toFixed(1),
+              farenheit: ((res.data.main.temp - 273.15) * 9/5 + 32).toFixed(1)
+            }
+            setTemperature(obj)
+          })
+        .catch(err => console.log(err))
+        .finally(() => setTimeout(() => setIsLoading(false), 2000))
+    }
+  }, [coords])
+  
+  
+console.log(weather)
+
+  return (
+    <div className="App">
+     { 
+     isLoading ?
+      <div className='load_screen'>
+      <img className='load_image' src="/src/assets/logo.png" alt="" />
+      </div>
+     :
+     <WeatherCard 
+        weather={weather}
+        temperature={temperature}
+      />
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const WeatherCard = ( {weather, temperature} ) => {
+
+  const [isCelsius, setIsCelsius] = useState(true)
+
+  const handleClick = () => setIsCelsius(!isCelsius)
+
+  return (
+    <article className='card'>
+      <h1 className='tittle ff-shadow'>Weather App</h1>
+      <h2 className='country ff-roboto'>{weather?.name}, {weather?.sys.country}</h2>
+      <div className='weather_information ff-roboto'>
+        <div className='weather_image'>
+          <img className='image' src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt="" />
+          <h2 className='temperature'><i class='bx icon_temp bxs-thermometer' ></i>{isCelsius ? temperature?.celsius + '°C': temperature?.farenheit + '°F'}</h2>
+        </div>
+        <div className='information'>
+          <section>
+            <h3 className='back_tittle'>"{weather?.weather[0].description}"</h3>
+            <ul>
+              <li className='data'><i class='bx icon_hum bxs-droplet'></i><span>  Humidity: </span>{weather?.main.humidity}%</li>
+              <li className='data'><i class='bx icon_wind bxl-tailwind-css'></i><span> Wind Speed: </span>{weather?.wind.speed}m/s</li>
+              <li className='data'><i class='bx icon_clouds bxs-cloud'></i><span> Clouds: </span>{weather?.clouds.all}%</li>
+              <li className='data'><i class='bx icon_press bxs-tachometer'></i><span> Pressure: </span>{weather?.main.pressure}hPa</li>
+            </ul>
+          </section>
+        </div>
+      </div>
+      <footer>
+        <button className='ff-roboto' onClick={handleClick}>Change to °{isCelsius ? 'F' : 'C'}</button>
+      </footer>
+    </article>
+  )
+}
+
+export default WeatherCard
